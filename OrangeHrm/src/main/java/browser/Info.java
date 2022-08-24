68025e82f25fbee2c73d25eb502c8a97c35d533d
yuva <feltanejuba97@gmail.com>
2022-08-23 22:13:13 -0400

    addinf class

---
+++ OrangeHrm/src/main/java/browser/Info.java
@@ -0,0 +1,8 @@
+package browser;
+
+public class Info {
+    public final static String URL="https://opensource-demo.orangehrmlive.com/";
+        static   String Browser ="chrome";
+    public final static String USER_NAME= "Admin";
+    public final static String PASSWORD ="admin123";
+}
