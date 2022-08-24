3c2531b3cf16127d4a84be213cf34c69942510cc
yuva <feltanejuba97@gmail.com>
2022-08-24 17:07:20 -0400

    adding changes

--- OrangeHrm/src/main/java/browser/Browser.java
+++ OrangeHrm/src/main/java/browser/Browser.java
@@ -3,6 +3,7 @@ package browser;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Browser {
